10cd63610ef96fdc0da954b55f7152ae91695842
Chuks200 <agbataekwechuks@gmail.com>
2024-06-22 01:24:40 +0100

    calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,99 @@
-import "./App.css"
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [input1, setInput1] = useState('0');
+  const [input2, setInput2] = useState('0');
+  const [operation, setOperation] = useState('+');
+  const [result, setResult] = useState('0');
+  const [storedValue, setStoredValue] = useState('0');
+
+  const handleNumberClick = (number, setInput, input) => {
+    setInput(input === '0' ? number : input + number);
+  };
+
+  const handleOperationClick = (op) => {
+    setOperation(op);
+  };
+
+  const handleEqualClick = () => {
+    const num1 = parseFloat(input1);
+    const num2 = parseFloat(input2);
+    let res;
+    switch (operation) {
+      case '+':
+        res = num1 + num2;
+        break;
+      case '-':
+        res = num1 - num2;
+        break;
+      case '*':
+        res = num1 * num2;
+        break;
+      case '/':
+        res = num1 / num2;
+        break;
+      default:
+        res = 0;
+    }
+    setResult(res.toString());
+  };
+
+  const handleClearClick = (setInput) => {
+    setInput('0');
+  };
+
+  const handleStoreClick = () => {
+    setStoredValue(result);
+  };
+
+  const handleRecallClick = (setInput) => {
+    setInput(storedValue);
+  };
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{input1}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
+            <button key={num} onClick={() => handleNumberClick(num.toString(), setInput1, input1)}>
+              {num}
+            </button>
+          ))}
+          <button onClick={() => handleClearClick(setInput1)}>Clear</button>
+          <button onClick={() => handleRecallClick(setInput1)}>Recall</button>
         </div>
       </div>
-
       <div className="panel">
-        <p>+</p>
+        <p>{operation}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          {['+', '-', '*', '/'].map((op) => (
+            <button key={op} onClick={() => handleOperationClick(op)}>
+              {op}
+            </button>
+          ))}
         </div>
       </div>
-
       <div className="panel">
-        <p>0</p>
+        <p>{input2}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
+            <button key={num} onClick={() => handleNumberClick(num.toString(), setInput2, input2)}>
+              {num}
+            </button>
+          ))}
+          <button onClick={() => handleClearClick(setInput2)}>Clear</button>
+          <button onClick={() => handleRecallClick(setInput2)}>Recall</button>
         </div>
       </div>
-      <div className="panel answer">
-        <p>0</p>
-        <div>
-          <button>=</button>
-        </div>
+      <div className="answer">
+        <p>{result}</p>
+        <button onClick={handleEqualClick}>=</button>
+        <button onClick={handleStoreClick}>Store</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
